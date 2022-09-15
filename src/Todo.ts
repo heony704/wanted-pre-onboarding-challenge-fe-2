8bd5327c2f28c8a2bf47ff0a45dda433bd7eca42
SeungHe0n <014sh@naver.com>
2022-09-15 10:15:29 +0900

    jsdoc 주석 삭제

--- src/Todo.ts
+++ src/Todo.ts
@@ -7,13 +7,6 @@ export default class Todo {
     #category: string;
     #tags: Tags;
 
-    /**
-     * 새로운 Todo를 만든다.
-     * @param { number } id - Todo의 아이디
-     * @param { string } content - Todo의 내용 (내용이 무조건 있어야 한다)
-     * @param { string } category - Todo의 카테고리
-     * @param { string[] } [tags] - Todo의 태그들 (optional)
-     */
     constructor(id: number, content: string, category: string, tags: Tags = []) {
         this.#id = id;
         this.#content = content;
@@ -22,122 +15,56 @@ export default class Todo {
         this.#tags = tags;
     }
 
-    /**
-     * Todo의 아이디를 조회한다.
-     * @func
-     * @return { number } Todo의 아이디
-     */
     get id() {
         return this.#id;
     }
 
-    /**
-     * Todo의 내용을 조회한다.
-     * @func
-     * @return { string } Todo의 내용
-     */
     get content() {
         return this.#content;
     }
 
-    /**
-     * Todo의 카테고리를 조회한다.
-     * @func
-     * @return { string } Todo의 카테고리
-     */
     get category() {
         return this.#category;
     }
 
-    /**
-     * Todo의 태그들을 조회한다.
-     * @func
-     * @return { string[] } Todo의 태그들
-     */
     get tags() {
         return this.#tags;
     }
 
-    /**
-     * Todo의 완료여부를 조회한다.
-     * @func
-     * @return { bool } Todo의 완료여부
-     */
     get isDone() {
         return this.#isDone;
     }
 
-    /**
-     * Todo를 완료상태로 만든다.
-     * @func
-     */
     complete() {
         this.#isDone = true;
     }
 
-    /**
-     * Todo를 미완료상태로 만든다.
-     * @func
-     */
     incomplete() {
         this.#isDone = false;
     }
 
-    /**
-     * Todo의 내용을 수정한다.
-     * @func
-     * @param { string } newContent - 새로운 내용
-     */
     #editContent(newContent: string) {
         this.#content = newContent;
     }
 
-    /**
-     * Todo의 카테고리를 수정한다.
-     * @func
-     * @param { string } newCategory - 새로운 카테고리
-     */
     #editCategory(newCategory: string) {
         this.#category = newCategory;
     }
 
-    /**
-     * Todo의 특정 태그를 수정한다.
-     * @func
-     * @param { string } targetTag - 수정하려고 하는 태그 이름
-     * @param { string } newTag - 새로운 태그 이름
-     */
     #editTag(targetTag, newTag) {
         const targetTagIndex = this.#tags.indexOf(targetTag);
         this.#tags[targetTagIndex] = newTag;
     }
 
-    /**
-     * Todo의 특정 태그를 삭제한다.
-     * @func
-     * @param { string } targetTag - 삭제하려는 태그 이름
-     */
     #deleteTag(targetTag) {
         const targetTagIndex = this.#tags.indexOf(targetTag);
         this.#tags.splice(targetTagIndex, 1);
     }
 
-    /**
-     * Todo의 모든 태그를 삭제한다.
-     * @func
-     */
     #deleteAllTags() {
         this.#tags = [];
     }
 
-    /**
-     * Todo를 주어진 내용에 따라 수정한다.
-     * @func
-     * @param { Object } newTodo - 수정할 부분만 합쳐서 객체로 받음
-     * @param { string } newTodo.content - 수정할 내용
-     * @param { string } newTodo.category - 수정할 카테고리
-     * @param { string[] } newTodo.tags - 수정할 태그들
-     */
     edit(newTodo) {
         if (newTodo.content) this.#editContent(newTodo.content);
         if (newTodo.category) this.#editCategory(newTodo.category);
--- src/TodoList.ts
+++ src/TodoList.ts
@@ -4,53 +4,27 @@ export default class TodoList {
     #nextTodoId: number;
     #todos: Todo[];
 
-    /**
-     * Todo List를 만든다.
-     */
     constructor() {
         this.#nextTodoId = 1;
         this.#todos = [];
     }
 
-    /**
-     * TodoList에 Todo를 추가한다.
-     * @func
-     * @param { string } content - Todo의 내용 (내용이 무조건 있어야 한다)
-     * @param { string } category - Todo의 카테고리
-     * @param { string[] } [tags] - Todo의 태그들 (optional)
-     */
     addTodo(content, category, tags) {
         const newTodo = new Todo(this.#nextTodoId, content, category, tags);
         this.#todos.push(newTodo);
         this.#nextTodoId++;
     }
 
-    /**
-     * TodoList에서 해당 id를 가진 Todo를 조회한다.
-     * @func
-     * @param { string } id - 조회하려는 Todo의 아이디
-     * @return { Todo } 해당 id의 Todo
-     */
     getTodo(id) {
         for (let i=0; i<this.#todos.length; i++) {
             if (this.#todos[i].id === id) return this.#todos[i];
         }
     }
 
-    /**
-     * TodoList의 모든 Todo를 조회한다.
-     * @func
-     * @return { Todo[] } 모든 Todo 배열
-     */
     getAllTodos() {
         return this.#todos;
     }
 
-    /**
-     * TodoList에서 해당 id를 가진 Todo를 삭제한다.
-     * @func
-     * @param { string } id - 삭제하려는 Todo의 아이디
-     */
     deleteTodo(id) {
         let targetTodoIndex = -1;
         for (let i=0; i<this.#todos.length; i++) {
@@ -60,10 +34,6 @@ export default class TodoList {
         this.#todos.splice(targetTodoIndex, 1);
     }
 
-    /**
-     * TodoList의 모든 Todo를 삭제한다.
-     * @func
-     */
     deleteAllTodos() {
         this.#todos = [];
     }
